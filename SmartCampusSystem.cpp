34700f0dc141aaa289428190070bb5d648364f18
annieain55 <ainnyyainunderscore55@gmail.com>
2026-06-28 14:32:32 +0500

    Added main function with test code

--- SmartCampusSystem.cpp
+++ SmartCampusSystem.cpp
@@ -1,20 +1,67 @@
-// SmartCampusSystem.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "Student.h"
+#include "GradStudent.h"
+#include "Faculty.h"
+#include "Course.h"
+#include "Enrollment.h"
+#include "Book.h"
+#include "Library.h"
+#include "FeeRecord.h"
+#include "Invoice.h"
+#include "Room.h"
+#include "HostelBlock.h"
+#include "HostelManager.h"
+#include "Utils.h"
+#include "Reports.h"
+#include "Exceptions.h"
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    Student s1("Hoorain", "12345-1234567-1", 19, "0300-1111111", 188, 2, 3.7f);
+    s1.show();
+
+    GradStudent gs("Ali", "11111-1111111-1", 24, "0300-2222222", 500, 6, 3.9f, "AI in Healthcare", "Dr. Khan");
+    gs.show();
+
+    Faculty f1("Dr. Khan", "22222-2222222-2", 40, "0300-3333333", 101, "CS", "Professor");
+    f1.show();
+
+    Course c1("CS104", "OOP", 3, &f1, 2);
+    try {
+        c1.enroll();
+        c1.enroll();
+        c1.enroll();
+    }
+    catch (CapacityExceededException& e) {
+        cout << "Exception: " << e.what() << endl;
+    }
+    cout << c1 << endl;
+
+    Book b1("B001", "Clean Code", "Robert Martin", 2008, "978-1", "Programming", 2);
+    b1.checkout();
+
+    Library lib;
+    lib.addItem(&b1);
+    LibraryItem* found = lib.searchByTitle("Clean Code");
+    if (found) cout << "Found: " << found->getTitle() << endl;
+
+    FeeRecord fee("S188", 50000, 10000, 500);
+    fee -= 20000;
+    cout << "Remaining Balance: " << fee.getBalance() << endl;
+
+    Invoice inv1("2026-06-27", 60500);
+    cout << "Invoice ID: " << inv1.getId() << endl;
+
+    Room r1(101, "Triple", 1);
+    r1.addOccupant(&s1);
+    HostelBlock blockA("Block A");
+    blockA.addRoom(&r1);
+    HostelManager hm(&blockA);
+    hm.allocateRoom();
+    cout << hm.generateReport() << endl;
+
+    cout << "Grade: " << s1.calcGrade() << endl;
+    cout << "Valid Age: " << Utils::isValidAge(s1.getAge()) << endl;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
